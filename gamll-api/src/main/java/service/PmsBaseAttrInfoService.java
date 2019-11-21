89737e891726fc231dadaf1f2f2e068e4f4ae56b
jiangtianlun <jiangtianlun123@163.com>
2019-11-21 09:15:41 +0800

    gmall-user-service 端口8070 gmall-user-web     端口8080 gmall-manager-service 端口8071 gmall-manager-web 端口8081

--- gamll-api/src/main/java/service/PmsBaseAttrInfoService.java
+++ gamll-api/src/main/java/service/PmsBaseAttrInfoService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface PmsBaseAttrInfoService {
     List<PmsBaseAttrInfo> getById(String catalog3Id);
+    boolean savePmsBaseAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo);
 }
---
+++ gamll-api/src/main/java/service/PmsProductInfoService.java
@@ -0,0 +1,9 @@
+package service;
+
+import bean.PmsProductInfo;
+
+import java.util.List;
+
+public interface PmsProductInfoService {
+   public List<PmsProductInfo> getAll();
+}
